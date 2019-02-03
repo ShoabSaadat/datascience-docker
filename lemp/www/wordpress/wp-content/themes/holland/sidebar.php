dae04013d31e2d509d23163de14b4f79ca1518c7
Shoaib Saadat <dr.shoaibsaadat@gmail.com>
2019-02-03 18:58:13 +0000

    Thinning master

--- lemp/www/wordpress/wp-content/themes/holland/sidebar.php
+++
@@ -1,5 +0,0 @@
-<aside id="sidebar">
-    <?php if ( is_active_sidebar( 'holland-main-sidebar' ) ) : ?>
-		<?php dynamic_sidebar( 'holland-main-sidebar' ); ?>
-	<?php endif; ?>
-</aside><!--sidebar-->
